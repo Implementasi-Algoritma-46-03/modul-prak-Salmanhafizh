798b3ac528e8c26d1d9416f04fb2ab36b191c123
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-09-27 10:54:17 +0700

    feat: Upload tugas TP01

--- Modul03/TP01.java
+++ Modul03/TP01.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class TP01 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner scanner = new Scanner(System.in);
+        int awal = scanner.nextInt();
+        int tabung = scanner.nextInt();
+        int hari = scanner.nextInt();
+        int hasil = awal + (tabung * hari);
+        System.out.println(hasil);
+        scanner.close();
     }
 }
