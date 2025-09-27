f42c9c3b8b98585cc2e887498142c705fa4ac752
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-09-27 13:29:06 +0700

    Feat: TP03 challenge achieved

--- Modul03/TP03.java
+++ Modul03/TP03.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class TP03 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner scanner = new Scanner(System.in);
+        int jumlahPermen = scanner.nextInt();
+        int teman = scanner.nextInt();
+        teman += 1; // ditambah 1 untuk diri sendiri juga
+        int hasilBagi = jumlahPermen / teman;
+        int sisaBagi = jumlahPermen % teman;
+        System.out.println(hasilBagi);
+        System.out.println(sisaBagi);
+        scanner.close();
     }
 }
