7106821569687bf7163a6c61288df547f70d8e1f
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-09-29 07:23:41 +0700

    Kodingan TP01 Terbaru

--- Modul03/TP01.java
+++ Modul03/TP01.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class TP01 {
 
     public static void main(String[] args) {
-        int tabunganAwal = 250000;
-        int tabunganMasuk = 25000;
-        int hariMenabung = 30;
+        int tabunganAwal = 0;
+        int tabunganMasuk = 0;
+        int hariMenabung = 0;
         int hasilTabungan = 0;
 
-        hasilTabungan += tabunganAwal + (tabunganMasuk * hariMenabung);
+        Scanner scanner = new Scanner(System.in);
+        tabunganAwal = scanner.nextInt();
+        tabunganMasuk = scanner.nextInt();
+        hariMenabung = scanner.nextInt();
+        hasilTabungan = tabunganAwal + (tabunganMasuk * hariMenabung);
         System.out.println(hasilTabungan);
+        scanner.close();
     }
 }
