ead5b3d6d30fdc71addbc48c6d85c9ab6f686d01
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-09-29 08:26:08 +0700

    mengembalikan ke versi sebelumnya

--- Modul03/TP01.java
+++ Modul03/TP01.java
@@ -3,16 +3,12 @@ import java.util.Scanner;
 public class TP01 {
 
     public static void main(String[] args) {
-        int tabunganAwal = 0;
-        int tabunganMasuk = 0;
-        int hariMenabung = 0;
-        int hasilTabungan = 0;
 
         Scanner scanner = new Scanner(System.in);
-        tabunganAwal = scanner.nextInt();
-        tabunganMasuk = scanner.nextInt();
-        hariMenabung = scanner.nextInt();
-        hasilTabungan = tabunganAwal + (tabunganMasuk * hariMenabung);
+        int tabunganAwal = scanner.nextInt();
+        int tabunganMasuk = scanner.nextInt();
+        int hariMenabung = scanner.nextInt();
+        int hasilTabungan = tabunganAwal + (tabunganMasuk * hariMenabung);
         System.out.println(hasilTabungan);
         scanner.close();
     }
