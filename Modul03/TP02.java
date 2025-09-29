eed6dba64904c9c553c435e7464dd4bf9a12d784
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-09-29 07:28:09 +0700

    Upload Modul03 (hopefully this is the last)

--- Modul03/TP02.java
+++ Modul03/TP02.java
@@ -1,19 +1,13 @@
 import java.util.Scanner;
 
 public class TP02 {
-
     public static void main(String[] args) {
-        String namaPembeli;
-        int jumlahBelanjaan = 0;
-        int hargaBelanjaan = 0;
-        int totalBelanjaan = 0;
-
         Scanner scanner = new Scanner(System.in);
-        namaPembeli = scanner.next();
-        jumlahBelanjaan += scanner.nextInt();
-        hargaBelanjaan += scanner.nextInt();
-        totalBelanjaan += jumlahBelanjaan * hargaBelanjaan;
-        System.out.println("Hi " + namaPembeli + ", total belanja adalah " + totalBelanjaan + " rupiah.");
+        String namaPembeli = scanner.next();
+        int jumlahBelanjaan = scanner.nextInt();
+        int hargaBelanjaan = scanner.nextInt();
+        int totalBelanjaan = jumlahBelanjaan * hargaBelanjaan;
+        System.out.println("Hi, " + namaPembeli + ". Total belanja adalah " + totalBelanjaan + " rupiah.");
         scanner.close();
     }
 }
--- Modul03/TP03.java
+++ Modul03/TP03.java
@@ -1,19 +1,13 @@
 import java.util.Scanner;
 
 public class TP03 {
-
     public static void main(String[] args) {
-        int jumlahPermen = 0;
-        int jumlahTeman = 0;
-        int hasilBagi = 0;
-        int sisaBagi = 0;
-
         Scanner scanner = new Scanner(System.in);
-        jumlahPermen += scanner.nextInt();
-        jumlahTeman += scanner.nextInt();
-        jumlahTeman += 1; // Tambah 1 untuk diri sendiri juga
-        hasilBagi += jumlahPermen / jumlahTeman;
-        sisaBagi += jumlahPermen % jumlahTeman;
+        int jumlahPermen = scanner.nextInt();
+        int jumlahTeman = scanner.nextInt();
+        int totalOrang = jumlahTeman + 1;
+        int hasilBagi = jumlahPermen / totalOrang;
+        int sisaBagi = jumlahPermen % totalOrang;
         System.out.println(hasilBagi);
         System.out.println(sisaBagi);
         scanner.close();
