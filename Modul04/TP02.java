6c418a9305b51a197da6ce14c337d2b2ec7e9b9d
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-10-04 22:22:41 +0700

    Upload Tugas TP Modul04

--- Modul04/TP02.java
+++ Modul04/TP02.java
@@ -1,6 +1,32 @@
-public class TP02 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class TP02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int uangTransaksi = scanner.nextInt();
+        double discount = 0.05;
+        double ppn = 0.11;
+        int hasilDiscount = 0;
+        int total;
+        double hasilPajak;
+        double totalBayar;
+
+        System.out.println("Transaksi = " + uangTransaksi);
+
+        if (uangTransaksi > 200000) {
+            hasilDiscount = (int) (uangTransaksi * discount);
+            total = uangTransaksi - hasilDiscount;
+            System.out.println("Total = " + uangTransaksi + " - " + hasilDiscount);
+        } else {
+            total = uangTransaksi;
+            System.out.println("Total = " + total);
+        }
+
+        hasilPajak = total * ppn;
+        totalBayar = total + hasilPajak;
+
+        System.out.printf("Ppn 11%% = %.1f\n", hasilPajak);
+        System.out.printf("Total dibayar = %.1f\n", totalBayar);
+        scanner.close();
     }
 }
--- Modul04/TP03.java
+++ Modul04/TP03.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class TP03 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner scanner = new Scanner(System.in);
+        int dira = scanner.nextInt();
+        int radi = scanner.nextInt();
+        int idar = scanner.nextInt();
+
+        if (dira > radi && dira > idar) {
+            System.out.println("Dira");
+        } else if (radi > dira && radi > idar) {
+            System.out.println("Radi");
+        } else if (idar > radi && idar > dira) {
+            System.out.println("Idar");
+        }
+
+        scanner.close();
     }
 }
