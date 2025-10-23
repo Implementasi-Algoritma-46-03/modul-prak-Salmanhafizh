74d5e522a6361076c9ad33492caf54a25f1cc26d
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-10-23 17:21:05 +0700

    Jurnal 6 last meureun

--- Modul06/Jurnal02.java
+++ Modul06/Jurnal02.java
@@ -8,49 +8,30 @@ public class Jurnal02 {
         String lirikBaru = "";
         switch (sisaLirik) {
             case 0:
+                lirikBaru = lirik.replaceAll("[IUEO]", "A");
                 lirikBaru = lirik.replaceAll("[iueo]", "a");
                 break;
             case 1:
-                // lirikBaru = lirik.replaceAll("[AIUEO]", "E");
+                lirikBaru = lirik.replaceAll("[AIUO]", "E");
                 lirikBaru = lirik.replaceAll("[aiuo]", "e");
                 break;
             case 2:
-                // lirikBaru = lirik.replaceAll("[AIUEO]", "I");
+                lirikBaru = lirik.replaceAll("[AUEO]", "I");
                 lirikBaru = lirik.replaceAll("[aueo]", "i");
                 break;
             case 3:
-                // lirikBaru = lirik.replaceAll("[AIUEO]", "O");
+                lirikBaru = lirik.replaceAll("[AIUE]", "O");
                 lirikBaru = lirik.replaceAll("[aiue]", "o");
                 break;
             case 4:
-                // lirikBaru = lirik.replaceAll("[AIUEO]", "U");
+                lirikBaru = lirik.replaceAll("[AIEO]", "U");
                 lirikBaru = lirik.replaceAll("[aieo]", "u");
                 break;
 
             default:
                 break;
         }
-
-        switch (sisaLirik) {
-            case 0:
-                lirikBaru = lirik.replaceAll("[IUEO]", "A");
-                break;
-            case 1:
-                lirikBaru = lirik.replaceAll("[AIUO]", "E");
-                break;
-            case 2:
-                lirikBaru = lirik.replaceAll("[AUEO]", "I");
-                break;
-            case 3:
-                lirikBaru = lirik.replaceAll("[AIUE]", "O");
-                break;
-            case 4:
-                lirikBaru = lirik.replaceAll("[AIEO]", "U");
-                break;
-                
-            default:
-                break;
-        }
+        
         return lirikBaru;
     }
     public static void main(final String[] args) {
