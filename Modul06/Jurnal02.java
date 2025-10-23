0d37e1460e601fbb2b28f64bd33822e4deaae6c2
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-10-23 17:14:15 +0700

    Jurnal 6

--- Modul06/Jurnal02.java
+++ Modul06/Jurnal02.java
@@ -11,23 +11,25 @@ public class Jurnal02 {
                 lirikBaru = lirik.replaceAll("[aiueo]", "a");
                 break;
             case 1:
+                // lirikBaru = lirik.replaceAll("[AIUEO]", "E");
                 lirikBaru = lirik.replaceAll("[aiueo]", "e");
                 break;
             case 2:
+                // lirikBaru = lirik.replaceAll("[AIUEO]", "I");
                 lirikBaru = lirik.replaceAll("[aiueo]", "i");
                 break;
             case 3:
+                // lirikBaru = lirik.replaceAll("[AIUEO]", "O");
                 lirikBaru = lirik.replaceAll("[aiueo]", "o");
                 break;
             case 4:
+                // lirikBaru = lirik.replaceAll("[AIUEO]", "U");
                 lirikBaru = lirik.replaceAll("[aiueo]", "u");
                 break;
 
             default:
                 break;
         }
-
-        
 
         switch (sisaLirik) {
             case 0:
