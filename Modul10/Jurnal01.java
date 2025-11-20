a24cf29b457124ed35253a5dfa08f2c62ba2da5c
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-11-20 16:34:39 +0700

    Update Jurnal

--- Modul10/Jurnal01.java
+++ Modul10/Jurnal01.java
@@ -2,20 +2,19 @@ public class Jurnal01 {
 
     public static void main(final String[] args) {
         for (int i = 1; i <= 6; i++) {
-            if (i % 2 == 0) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= 5; j++) {
+            if (i % 2 == 0){
+                for (int j = 1; j <= 5; j++) {
+                    System.out.print(" " + i);
+                }
+            } else {
+                for (int j = 1; j <= 5; j++) {
                 System.out.print(i + " ");
             }
-            if (i % 2 != 0) {
-                System.out.print(" ");
             }
+        }
             System.out.println();
-            
+
         }
         // Kerjakan soalnya di sini
 
     }
-
-}
