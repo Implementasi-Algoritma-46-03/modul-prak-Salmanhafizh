19e1a24a07e97d0a7434db62f947e47a959a1ed4
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-11-20 16:27:02 +0700

    Update Jurnal dan TP modul 10

--- Modul10/Jurnal03.java
+++ Modul10/Jurnal03.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Jurnal03 {
 
     public static void main(final String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int angka = scanner.nextInt();
+        int hitung = 0;
+
+        for (int i = angka; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(hitung);
+                hitung++;
+                if (hitung == 10) {
+                    hitung = 0;
+                }
+            }
+            System.out.println();
+        }
+        scanner.close();
         // Kerjakan soalnya di sini
     }
 }
