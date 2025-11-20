8e8ab9a7e237a3457bb6e612405cccbd7c6a2996
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-11-20 16:37:33 +0700

    Update Jurnal

--- Modul10/Jurnal01.java
+++ Modul10/Jurnal01.java
@@ -8,7 +8,7 @@ public class Jurnal01 {
                 }
             } else {
                 for (int j = 1; j <= 5; j++) {
-                    System.out.print(i + " ");
+                    System.out.print(" " + i);
                 }
             }
             System.out.println();
