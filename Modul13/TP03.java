419544cb04821d3b813e89af5ae634bcb56a001b
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-12-08 15:30:48 +0700

    Push TP modul 13

--- Modul13/TP03.java
+++ Modul13/TP03.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
+
 public class TP03 {
 
     public static void main(final String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int jumlahMahasiswa = scanner.nextInt();
+        int[] array = new int[jumlahMahasiswa];
+        for (int i = 0; i < jumlahMahasiswa; i++) {
+            int input = scanner.nextInt();
+            array[i] = input;
+        }
+        int target = scanner.nextInt();
+
+        int hasil = lookForIt(array, target);
+
+        if (hasil != -1) {
+            System.out.print("Ditemukan di indeks ke-" + hasil);
+        } else {
+            System.out.print("Tidak ditemukan");
+        }
+
+        scanner.close();
         // Kerjakan soalnya di sini
+
+    }
+
+    public static int lookForIt(int[] array, int target) {
+        int length = array.length;
+        int hasil = 0;
+        for (int i = 0; i < length; i++) {
+            if (target == array[i]) {
+                hasil = i;
+                return hasil;
+            }
+        }
+        return -1;
     }
 }
