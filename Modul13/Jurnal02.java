14a671afe41cc41f384a5f2f8c1e1058a59256c7
Salmanhafizh <hfzslmnhaq2@gmail.com>
2025-12-11 14:56:55 +0700

    Upload Jurnal01 02 03

--- Modul13/Jurnal02.java
+++ Modul13/Jurnal02.java
@@ -1,6 +1,50 @@
-public class Jurnal02 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int jumlahArray = scanner.nextInt();
+        String[] arrayString = new String[jumlahArray];
+
+        for (int i = 0; i < jumlahArray; i++) {
+            arrayString[i] = scanner.next().trim().toUpperCase();
+        }
+
+        sortArray(arrayString);
+
+        for(int i = 0; i < jumlahArray; i++){
+            String hasil = arrayString[i].substring(0, 1).toUpperCase() + arrayString[i].substring(1).toLowerCase();
+            System.out.print(hasil + (i + 1 == jumlahArray ? "" : " "));
+        }
+        scanner.close();
+    }
+    
+    public static void sortArray(String[] array) {
+        int panjang = array.length;
+        for (int i = 0; i < panjang - 1; i++) {
+            for (int j = 0; j < panjang - i - 1; j++) {
+                if (compareStrings(array[j], array[j + 1]) > 0) {
+                    String tempString = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tempString;
+                }
+            }
+        }
+    }
+    
+    public static int compareStrings(String s1, String s2){
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int minLen = Math.min(len1, len2);
+
+        for(int i = 0; i < minLen; i++){
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+
+            if(c1 != c2){
+                return c1 - c2;
+            }
+        }
+        return len1 - len2;
     }
 }
--- Modul13/Jurnal03.java
+++ Modul13/Jurnal03.java
@@ -1,6 +1,34 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int jumlahArray = scanner.nextInt();
+        String[] arrayString = new String[jumlahArray];
+
+        for (int i = 0; i < jumlahArray; i++) {
+            arrayString[i] = scanner.next().trim();
+        }
+        String cari = scanner.next().trim();
+
+        int hasil = cariArray(arrayString, cari);
+
+        if (hasil != -1) {
+            System.out.print("Ditemukan di indeks ke-" + hasil);
+        } else {
+            System.out.print("Data tidak ditemukan");
+        }
+        
+        scanner.close();
     }
+    
+    public static int cariArray(String[] array, String kata) {
+    int panjangArray = array.length;
+    for (int i = 0; i < panjangArray; i++) {
+        if (array[i].equalsIgnoreCase(kata)) {
+            return i;
+        }
+    }
+    return -1;
+}
 }
